a9d2f2db570237e579fe243d95bedd67ec10bf16
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-04-24 18:53:47 +1000

    chore(clean): removed some files

--- src/tests/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render } from "@testing-library/react"
-import App from "../App"
-
-test("renders learn react link", () => {
-	const { getByText } = render(<App />)
-	const heading = getByText(/What up!/i)
-	expect(heading).toBeInTheDocument()
-})
