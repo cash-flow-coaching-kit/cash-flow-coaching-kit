959c2ed04415d8dce649af0115e6fbd3f54ec614
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-04-28 15:47:33 +1000

    feat(client): sync client state with db on load

--- src/components/Routing/Router.tsx
+++ src/components/Routing/Router.tsx
@@ -1,7 +1,9 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement, useEffect, useContext } from "react"
 import { HashRouter as Router, Switch } from "react-router-dom"
 import PublicRoutesWrapper from "./Public"
 import PrivateRoutesWrapper from "./Private"
+import syncClientsWithDb from "../../data/client/syncWithDB"
+import { ClientContext } from "../../state/client"
 
 /**
  * Application router component
@@ -9,6 +11,14 @@ import PrivateRoutesWrapper from "./Private"
  * @returns {ReactElement}
  */
 const AppRouter = (): ReactElement => {
+	const { dispatch } = useContext(ClientContext)
+
+	useEffect(() => {
+		;(async function sync(): Promise<void> {
+			await syncClientsWithDb(dispatch)
+		})()
+	}, [dispatch])
+
 	return (
 		<Router>
 			<Switch>
