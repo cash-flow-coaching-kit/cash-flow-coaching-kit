1b5ef64692de09d15011e7010f322c27f027b464
akte-akte <kate@annex.com.au>
2020-04-29 17:34:17 +1000

    feat(discover topics): add selling-closing page

---
+++ src/pages/DiscoverTopics/selling-closing-succession.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from "react"
+import { PrivatePage } from "../../components/Layouts"
+import PageTip from "../../components/PageTip"
+
+const DTSellingClosingSuccession = (): ReactElement => {
+	return (
+		<PrivatePage>
+			<h1>DTSellingClosingSuccession</h1>
+			<PageTip tip="DiscoverTopicsTips" />
+		</PrivatePage>
+	)
+}
+
+export default DTSellingClosingSuccession
