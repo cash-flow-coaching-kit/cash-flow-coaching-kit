deb4350dec6d9d7ee9124faf5229bf1eb84dced8
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-04-29 16:18:06 +1000

    feat(navbar): show current client in the primary nav

--- src/components/Navbar/Primary/PrimaryNavbar.tsx
+++ src/components/Navbar/Primary/PrimaryNavbar.tsx
@@ -1,9 +1,10 @@
-import React, { ReactElement } from "react"
-import { AppBar, Box, Toolbar, Button } from "@material-ui/core"
+import React, { ReactElement, useContext } from "react"
+import { AppBar, Box, Toolbar, Button, Typography } from "@material-ui/core"
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
 import { useSharedNavStyles } from "../shared/style"
-import routes from "./config"
+import routes, { usePrimaryStyles } from "./config"
 import { NavigationRoutes } from "../shared/NavigationLinks"
+import { ClientContext } from "../../../state/client"
 
 /**
  * Renders the primary navigation
@@ -12,11 +13,27 @@ import { NavigationRoutes } from "../shared/NavigationLinks"
  */
 const PrimaryNavbar = (): ReactElement => {
 	const sharedStyle = useSharedNavStyles()
+	const styles = usePrimaryStyles()
+	const {
+		state: { currentClient },
+	} = useContext(ClientContext)
 
 	return (
 		<div className={sharedStyle.root}>
 			<AppBar position="static" color="default">
 				<Toolbar>
+					{currentClient ? (
+						<Box>
+							<Typography className={styles.clientCaption} variant="caption">
+								Client
+							</Typography>
+							<Typography className={styles.clientName} variant="h6">
+								{currentClient.name}
+							</Typography>
+						</Box>
+					) : (
+						false
+					)}
 					<Box className={sharedStyle.box}>
 						<NavigationRoutes
 							routes={routes}
