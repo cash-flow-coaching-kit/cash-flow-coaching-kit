4becfeff81d4bff7fd209b495c9528a436c3b055
akte-akte <kate@annex.com.au>
2020-04-30 11:36:51 +1000

    feat(discover topics): add grid list to funding page

--- src/pages/DiscoverTopics/funding-business.tsx
+++ src/pages/DiscoverTopics/funding-business.tsx
@@ -10,6 +10,7 @@ import {
 	CardMedia,
 	Button,
 } from "@material-ui/core"
+import { SectionTitle } from "../../components/Content"
 import { PrivatePage, PageContainer } from "../../components/Layouts"
 import PageTip from "../../components/PageTip"
 
@@ -21,6 +22,9 @@ const useDTStyles = makeStyles((theme) => ({
 		paddingTop: theme.spacing(5),
 	},
 	grid: {
+		paddingTop: theme.spacing(5),
+	},
+	SectionTitle: {
 		paddingTop: theme.spacing(5),
 	},
 	embed: {
@@ -70,7 +74,7 @@ const DTFundingBusiness = (): ReactElement => {
 							<CardMedia
 								className={styles.embed}
 								component="iframe"
-								src="https://youtu.be/fAe_2eyAkrU"
+								src="https://www.youtube.com/embed/fAe_2eyAkrU"
 							/>
 							<CardActions>
 								<Button color="primary">Download Transcript</Button>
@@ -83,7 +87,7 @@ const DTFundingBusiness = (): ReactElement => {
 							<CardMedia
 								className={styles.embed}
 								component="iframe"
-								src="https://youtu.be/BTwVYKfGyuk"
+								src="https://www.youtube.com/embed/BTwVYKfGyuk"
 							/>
 							<CardActions>
 								<Button color="primary">Download Transcript</Button>
@@ -91,6 +95,117 @@ const DTFundingBusiness = (): ReactElement => {
 						</Card>
 					</Grid>
 				</Grid>
+				<SectionTitle className={styles.SectionTitle}>
+					The four main types of funding
+				</SectionTitle>
+				<Grid container spacing={3}>
+					<Grid item xs={6}>
+						<Typography component="h3" variant="h5">
+							Debt Funding
+						</Typography>
+						<Typography component="p">
+							Money provided by an external party such as a bank that requires
+							repayment
+						</Typography>
+						<List>
+							<Typography component="li">
+								Financial Institutions such as a bank loan, line of credit, or
+								equipment lease.
+							</Typography>
+							<Typography component="li">
+								Retailers that offer the use of store credit to purchase goods
+								or equipment.
+							</Typography>
+							<Typography component="li">
+								Suppliers that allow you to delay your payment of goods and
+								services.
+							</Typography>
+							<Typography component="li">
+								Factor Companies are third parties that buy your unpaid
+								invoices, giving you cash now.
+							</Typography>
+							<Typography component="li">
+								Family or friends who are willing to loan cash under varied
+								circumstances and agreements.
+							</Typography>
+						</List>
+					</Grid>
+					<Grid item xs={6}>
+						<Typography component="h3" variant="h5">
+							Equity Funding
+						</Typography>
+						<Typography component="p">
+							Money sourced internally or from others, often for a stake of the
+							business.
+						</Typography>
+						<List>
+							<Typography component="li">
+								Self-funding (boot strapping) the initial phases of your
+								business with your own finances.
+							</Typography>
+							<Typography component="li">
+								Friends or family involved in the business by providing funding,
+								often through a partnership arrangement.
+							</Typography>
+							<Typography component="li">
+								Private investors contribute for a share of the profits or a
+								percentage stake in the business.
+							</Typography>
+							<Typography component="li">
+								Venture capitalists, such as large corporates or wealthy
+								individuals, who invest large sums of money for a part of the
+								business or future profit.
+							</Typography>
+							<Typography component="li">
+								Floating the company on the Stock market (Initial Public
+								Offering) in return for a share of profits.
+							</Typography>
+						</List>
+					</Grid>
+					<Grid item xs={6}>
+						<Typography component="h3" variant="h5">
+							Grant Funding
+						</Typography>
+						<Typography component="p">
+							Money granted for specific business purposes.
+						</Typography>
+						<List>
+							<Typography component="li">
+								Government grants can be given at a federal, state or local
+								level, for businesses that qualify under certain conditions.
+							</Typography>
+							<Typography component="li">
+								Private companies or organisations may offer grants to certain
+								causes or businesses as a philanthropic action or establishing
+								mutually beneficial ongoing relationships.
+							</Typography>
+						</List>
+					</Grid>
+					<Grid item xs={6}>
+						<Typography component="h3" variant="h5">
+							Crowdfunding
+						</Typography>
+						<Typography component="p">
+							Money raised for a business by a group or community of supporters
+							who contribute to a capital pool of funding.
+						</Typography>
+						<List>
+							<Typography component="li">
+								Multiple platforms facilitate crowdfunding, often asking that
+								funders, or “backers” be rewarded for their contribution should
+								the project come to fruition.
+							</Typography>
+							<Typography component="li">
+								It is important to understand the tax implications of a
+								crowdfunded business, as well as your liability for delivering
+								to "backers".
+							</Typography>
+						</List>
+					</Grid>
+				</Grid>
+				<SectionTitle className={styles.SectionTitle}>
+					QUICKSNAPS - What to look for when funding your business.
+				</SectionTitle>
 			</PageContainer>
 
 			<PageTip tip="DiscoverTopicsTips" />
