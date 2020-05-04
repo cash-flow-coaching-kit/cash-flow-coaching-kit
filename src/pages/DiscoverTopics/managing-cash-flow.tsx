6ca6d8556156e070b2708bbac014326b2d0f62bf
akte-akte <kate@annex.com.au>
2020-05-04 14:34:10 +1000

    feat(discover topics): add content to manage cashflow

--- src/pages/DiscoverTopics/managing-cash-flow.tsx
+++ src/pages/DiscoverTopics/managing-cash-flow.tsx
@@ -1,11 +1,51 @@
 import React, { ReactElement } from "react"
-import { PrivatePage } from "../../components/Layouts"
+import {
+	Typography,
+	Grid,
+	makeStyles,
+	List,
+	Card,
+	CardHeader,
+	CardActions,
+	CardMedia,
+	Button,
+} from "@material-ui/core"
+import { PrivatePage, PageContainer } from "../../components/Layouts"
 import PageTip from "../../components/PageTip"
 
+const useDTStyles = makeStyles((theme) => ({
+	tagline: {
+		marginBottom: theme.spacing(4),
+	},
+	container: {
+		paddingTop: theme.spacing(5),
+	},
+	grid: {
+		paddingTop: theme.spacing(5),
+	},
+	embed: {
+		minHeight: "300px",
+		border: "none",
+	},
+}))
+
 const DTManagingCashFlow = (): ReactElement => {
+	const styles = useDTStyles()
+
 	return (
 		<PrivatePage>
-			<h1>DTManagingCashFlow</h1>
+			<PageContainer>
+				<Typography variant="h5" align="center" className={styles.tagline}>
+					Funding your business
+				</Typography>
+
+				<Typography variant="body1" component="p">
+					Finding the right funding option is important to the long term success
+					of your business. Understanding the various types of funding can help
+					you select the best option for your business when starting up,
+					expanding, or going through a time of limited cash flow.
+				</Typography>
+			</PageContainer>
 			<PageTip tip="DiscoverTopicsTips" />
 		</PrivatePage>
 	)
