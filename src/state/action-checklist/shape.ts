56c5018334326db85b1fec5bc5ac06151de78ebd
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-05-04 17:32:27 +1000

    feat(ac): add initial shape of action checklist context

---
+++ src/state/action-checklist/shape.ts
@@ -0,0 +1,42 @@
+import { Dispatch } from "react"
+
+/**
+ * Actions that can be performed for state changes
+ *
+ * @export
+ * @enum {number}
+ */
+export enum ActionChecklistActionTypes {
+	ChangeHideCompleted = "change_hide_completed",
+	ChangeReviewBy = "change_review_by",
+	UpdateDatabaseSync = "change_database_sync",
+}
+
+/**
+ * Defines the type:payload type pairing
+ * aka; what the structure of the payload should be
+ * given a certain type
+ *
+ * @export
+ * @type ActionChecklistReducerActions
+ */
+export type ActionChecklistReducerActions =
+	| { type: ActionChecklistActionTypes.ChangeHideCompleted; payload: boolean }
+	| { type: ActionChecklistActionTypes.ChangeReviewBy; payload: Date }
+	| {
+			type: ActionChecklistActionTypes.UpdateDatabaseSync
+			payload: boolean
+	  }
+
+/**
+ * Structure for the action checklist context
+ *
+ * @export
+ * @interface IActionChecklistState
+ */
+export interface IActionChecklistState {
+	hideCompleted: boolean
+	reviewBy: Date
+	databaseSyced: boolean
+	dispatch: Dispatch<ActionChecklistReducerActions>
+}
