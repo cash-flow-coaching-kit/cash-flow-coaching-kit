e3f7e29a1480917676c21da866ba3d8bad0c51f6
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-05-14 11:12:40 +1000

    test(db): allow for database testing

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,6 +4,13 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect"
 
+import Dexie from "dexie"
+import indexedDB from "fake-indexeddb"
+import FDBKeyRange from "fake-indexeddb/lib/FDBKeyRange"
+
+Dexie.dependencies.indexedDB = indexedDB
+Dexie.dependencies.IDBKeyRange = FDBKeyRange
+
 Object.defineProperty(global.window, "crypto", {
 	value: {
 		getRandomValues: jest.fn((x) => x.length.toString()),
