62d56dab01ae9e2f0b32c43974bb3431e73315e0
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-05-25 17:36:58 +1000

    test(cfc): utility method tests

---
+++ src/util/reduce/__tests__/math.test.ts
@@ -0,0 +1,52 @@
+import { sum, minusBy, divideBy, pipe, MathHOF, add } from "../math"
+
+describe("Unit tests for math functions", () => {
+  test("Summing some numbers", function() {
+    const nums = [1, 2, 3, 4]
+    expect(nums.reduce(sum())).toBe(10)
+  })
+
+  test("minus 10", function() {
+    const minus10 = minusBy(10)
+    expect(minus10(100)).toBe(90)
+  })
+
+  test("minus -10", function() {
+    const minus10 = minusBy(-10)
+    expect(minus10(100)).toBe(110)
+  })
+
+  test("divide by 2", function() {
+    const divideBy2 = divideBy(2)
+    expect(divideBy2(10)).toBe(5)
+  })
+
+  test("add 100", function() {
+    const add100 = add(100)
+    expect(add100(100)).toBe(200)
+  })
+
+  test("pipe", function() {
+    const minus4 = minusBy(4)
+    const divideBy2 = divideBy(2)
+    const sumArr = (arr: number[]): number => arr.reduce(sum())
+
+    const pipeValue: number = pipe<number[], number, any[]>(
+      sumArr,
+      minus4,
+      divideBy2
+    )([1, 2, 3, 4])
+
+    expect(pipeValue).toBe(3)
+  })
+
+  test("pipe inline", function() {
+    const value = pipe<number, number, any[]>(
+      add(10),
+      minusBy(10),
+      divideBy(2)
+    )(10)
+
+    expect(value).toBe(5)
+  })
+})
