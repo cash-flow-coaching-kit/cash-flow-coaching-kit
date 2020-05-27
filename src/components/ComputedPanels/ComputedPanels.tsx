f45eea5817002fa4c252e6cb683503793d8526bb
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-05-27 10:19:56 +1000

    feat(cfc): add component to show title, description and a value

---
+++ src/components/ComputedPanels/ComputedPanels.tsx
@@ -0,0 +1,47 @@
+import React, { ReactElement } from "react"
+import { Box, Typography } from "@material-ui/core"
+import { useInputWrapper } from "../CFC/__config/styles"
+import useDMIStyles from "../DescriptiveMoneyInput/__config/styles"
+
+/**
+ * Prop definition for the ComputedPanels component
+ *
+ * @interface ComputedPanelsProps
+ */
+interface ComputedPanelsProps {
+	title: string
+	description: string
+	value: string
+	mini?: boolean
+}
+
+/**
+ * Displays a title, description and a computed value
+ *
+ * @export
+ * @param {ComputedPanelsProps} {
+ * 	title,
+ * 	description,
+ * 	value,
+ * }
+ * @returns {ReactElement}
+ */
+export default function ComputedPanels({
+	title,
+	description,
+	value,
+	mini = false,
+}: ComputedPanelsProps): ReactElement {
+	const wrapperCls = useInputWrapper()
+	const dmiCls = useDMIStyles({ stacked: false, mini })()
+
+	return (
+		<Box className={`${wrapperCls.wrapper} ${dmiCls.root}`} alignItems="center">
+			<Box className={dmiCls.type}>
+				<Typography variant="h6">{title}</Typography>
+				<Typography>{description}</Typography>
+			</Box>
+			<Typography variant="h6">{value}</Typography>
+		</Box>
+	)
+}
