60b5a325dc86deccb50efe6095eb9327152bed8c
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-05-28 13:55:16 +1000

    feat(cfc): income tax box

---
+++ src/components/CFC/IncomeTax.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from "react"
+import { Box } from "@material-ui/core"
+import DescriptiveMoneyInput from "../DescriptiveMoneyInput"
+import { ControlProps } from "./__config/shape"
+import { useInputWrapper } from "./__config/styles"
+
+export default function IncomeTax({
+	value,
+	onChange,
+}: ControlProps<number>): ReactElement {
+	const cls = useInputWrapper()
+
+	return (
+		<Box className={`${cls.wrapper} ${cls.highlight}`}>
+			<DescriptiveMoneyInput
+				variant="outlined"
+				title="Income Tax or Company Tax"
+				description="Estimated amount to pay your tax; approximately 25% of your Cash Surplus."
+				value={value}
+				onChange={onChange}
+				name="incomeTax"
+				mini
+			/>
+		</Box>
+	)
+}
