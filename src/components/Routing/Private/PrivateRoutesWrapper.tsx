ab2ee9e8f7f2d5a71e3b9f125714bffa9d5440ef
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-03 13:54:04 +1000

    feat(cfc): add compare page to router | add page structure to page

--- src/components/Routing/Private/PrivateRoutesWrapper.tsx
+++ src/components/Routing/Private/PrivateRoutesWrapper.tsx
@@ -22,6 +22,7 @@ import { ActionChecklistProvider } from "../../../state/action-checklist"
 import { PrivatePage } from "../../Layouts"
 import CanvasEdit from "../../../pages/CashFlowCanvas/canvasEdit"
 import CFCListing from "../../../pages/CashFlowCanvas/listing"
+import CFCCompare from "../../../pages/CashFlowCanvas/compare"
 
 /**
  * Private route definitions, these pages should not be available
@@ -61,6 +62,9 @@ const PrivateRoutesWrapper = (): ReactElement => {
 			</Route>
 			<Route path={PrivateRoutes.CFCListing}>
 				<CFCListing />
+			</Route>
+			<Route path={PrivateRoutes.CFCCompare}>
+				<CFCCompare />
 			</Route>
 
 			{/*
--- src/pages/CashFlowCanvas/compare.tsx
+++ src/pages/CashFlowCanvas/compare.tsx
@@ -1,11 +1,25 @@
 import React, { ReactElement } from "react"
+import { Grid } from "@material-ui/core"
+import { PageContainer } from "../../components/Layouts"
+import SectionTitle from "../../components/SectionTitle"
+import CompareCanvases from "../../components/CFCCompare"
+import FourQuestions from "../../components/HealthCheck/FourQuestions"
+import ControlPanel from "../../components/CFC/ControlPanel"
 
-const CFCCompare = (): ReactElement => {
+export default function CFCCompare(): ReactElement {
 	return (
-		<>
-			<h1>CFCCompare</h1>
-		</>
+		<PageContainer>
+			<SectionTitle>Compare canvas fields</SectionTitle>
+			<Grid container spacing={3}>
+				<Grid item sm={9}>
+					<CompareCanvases />
+				</Grid>
+				<Grid item sm={3}>
+					<FourQuestions />
+					{/* TODO: Modify to show the correct items */}
+					<ControlPanel />
+				</Grid>
+			</Grid>
+		</PageContainer>
 	)
 }
-
-export default CFCCompare
