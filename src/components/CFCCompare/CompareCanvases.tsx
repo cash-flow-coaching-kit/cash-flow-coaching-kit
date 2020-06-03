35b1500f47f233e8e516c05dfa4f35cdd07a2f3c
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-03 13:58:22 +1000

    feat(cfc): add state machine to the compare canvases component

--- src/components/CFCCompare/CompareCanvases.tsx
+++ src/components/CFCCompare/CompareCanvases.tsx
@@ -1,8 +1,12 @@
 import React, { ReactElement } from "react"
 import { Divider } from "@material-ui/core"
+import { useMachine } from "@xstate/react"
 import CompareSelector from "./CompareSelector"
 import Spacer from "../Spacer"
 import CompareTable from "./CompareTable"
+import fetchMachine from "../Forms/CFC/__config/machine"
+import NoCanvases from "../CFC/NoCanvases"
+import Loading from "../Loading"
 
 /**
  * Component wrapper to display all the data in the compare table
@@ -11,13 +15,32 @@ import CompareTable from "./CompareTable"
  * @returns {ReactElement}
  */
 export default function CompareCanvases(): ReactElement {
-	return (
-		<>
-			<CompareSelector />
-			<Spacer />
-			<Divider />
-			<Spacer />
-			<CompareTable />
-		</>
-	)
+	const [stateMachine, changeState] = useMachine(fetchMachine)
+
+	/**
+	 * Renders the component based on the current state of the state machine
+	 *
+	 * @returns {ReactElement}
+	 */
+	function renderForStateMachine(): ReactElement {
+		switch (stateMachine.value) {
+			case "failure":
+				return <NoCanvases />
+			case "success":
+				return (
+					<>
+						<CompareSelector />
+						<Spacer />
+						<Divider />
+						<Spacer />
+						<CompareTable />
+					</>
+				)
+			case "loading":
+			default:
+				return <Loading />
+		}
+	}
+
+	return renderForStateMachine()
 }
