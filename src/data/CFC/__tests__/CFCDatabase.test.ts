fac72b50ee3330373d1b44c582769ba38ac4304f
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-04 09:13:24 +1000

    test(cfc): add test cases for counting records based on client

--- src/data/CFC/__tests__/CFCDatabase.test.ts
+++ src/data/CFC/__tests__/CFCDatabase.test.ts
@@ -1,8 +1,22 @@
 import CFCDB from "../CFCDatabase"
 import Dexie from "dexie"
 import CFCUseCase from "../CFCLogic"
+import { ClientId } from "../../_config/shape"
+import { initialValues } from "../../../components/Forms/CFC"
 
 describe("Unit tests for CFC database", () => {
+  const clientId: ClientId = 1
+  const item = {...initialValues, clientId}
+
+  beforeEach(async () => {
+    await CFCDB.canvases.clear()
+  })
+
+  afterAll(async () => {
+    await CFCDB.canvases.clear()
+    CFCDB.close()
+  })
+
   test("test instance type", function() {
     expect(CFCDB).toBeInstanceOf(Dexie)
     expect(typeof CFCDB.canvases).not.toBeUndefined()
@@ -11,5 +25,13 @@ describe("Unit tests for CFC database", () => {
   test("Use case can do something", async function() {
     const count = await CFCUseCase.syncWithDatabase()
     expect(count).toHaveLength(0)
+  })
+
+  test("Can count the values", async function() {
+    let count = await CFCUseCase.countClientRecords(clientId)
+    expect(count).toBe(0)
+    await CFCUseCase.create(item)
+    count = await CFCUseCase.countClientRecords(clientId)
+    expect(count).toBe(1)
   })
 })
