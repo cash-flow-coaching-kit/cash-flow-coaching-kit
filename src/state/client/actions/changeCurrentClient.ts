2a0240a01023b68ae274376abf64f9a76f458a84
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-05 16:26:38 +1000

    feat(client): 105 - remember the current client after reload

--- src/state/client/actions/changeCurrentClient.ts
+++ src/state/client/actions/changeCurrentClient.ts
@@ -1,12 +1,18 @@
 import { IClientState } from "../client-outline"
 import { ClientId } from "../../../data/_config/shape"
 import filterById from "../../../util/filters/ById"
+import {
+	setStorageClient,
+	emptyClientValue,
+} from "../../../util/localStorage/client"
 
 const changeCurrentClient = (
 	state: IClientState,
 	payload: ClientId
 ): IClientState => {
 	const client = state.state.clients.find(filterById(payload))
+	setStorageClient(client?.id || emptyClientValue)
+
 	return {
 		...state,
 		state: {
