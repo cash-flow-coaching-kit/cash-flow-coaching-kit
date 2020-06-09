802270e3670d67025773ed8a147b8274eefb9025
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-09 10:23:09 +1000

    feat(navbar): add logo component to the primary and public navbars

--- src/components/Navbar/Primary/PrimaryNavbar.tsx
+++ src/components/Navbar/Primary/PrimaryNavbar.tsx
@@ -1,11 +1,20 @@
 import React, { ReactElement, useContext } from "react"
-import { AppBar, Box, Toolbar, Button, Typography } from "@material-ui/core"
+import {
+	AppBar,
+	Box,
+	Toolbar,
+	Button,
+	Typography,
+	Grid,
+} from "@material-ui/core"
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
 import { useSharedNavStyles } from "../_config/style"
 import { NavigationRoutes } from "../_partials"
 import { ClientContext } from "../../../state/client"
 import usePrimaryStyles from "./_config/styles"
 import { routes } from "./_config/data"
+import Logo from "../../Logo"
+import { PrivateRoutes } from "../../../util/routes/routes"
 
 /**
  * Renders the primary navigation
@@ -23,34 +32,43 @@ const PrimaryNavbar = (): ReactElement => {
 		<div className={sharedStyle.root}>
 			<AppBar position="static" color="default">
 				<Toolbar>
-					{currentClient ? (
-						<Box>
-							<Typography className={styles.clientCaption} variant="caption">
-								Client
-							</Typography>
-							<Typography className={styles.clientName} variant="h6">
-								{currentClient.name}
-							</Typography>
-						</Box>
-					) : (
-						false
-					)}
-					<Box className={sharedStyle.box}>
-						<NavigationRoutes
-							routes={routes}
-							color="inherit"
-							className={sharedStyle.button}
-						/>
-
-						{/* TODO: Pull into a separate component to add modal trigger and content */}
-						<Button
-							className={sharedStyle.button}
-							color="inherit"
-							startIcon={<HelpOutlineIcon />}
+					<Grid container spacing={2}>
+						{currentClient ? (
+							<Grid item sm={4} className="">
+								<Typography className={styles.clientCaption} variant="caption">
+									Client
+								</Typography>
+								<Typography className={styles.clientName} variant="h6">
+									{currentClient.name}
+								</Typography>
+							</Grid>
+						) : (
+							<Grid item sm={4} />
+						)}
+						<Grid item sm={4} className={`${sharedStyle.logoBox}`}>
+							<Logo to={PrivateRoutes.CoachingKit} />
+						</Grid>
+						<Grid
+							item
+							sm={4}
+							className={`${sharedStyle.box} ${sharedStyle.small}`}
 						>
-							Help
-						</Button>
-					</Box>
+							<NavigationRoutes
+								routes={routes}
+								color="inherit"
+								className={sharedStyle.button}
+							/>
+
+							{/* TODO: Pull into a separate component to add modal trigger and content */}
+							<Button
+								className={sharedStyle.button}
+								color="inherit"
+								startIcon={<HelpOutlineIcon />}
+							>
+								Help
+							</Button>
+						</Grid>
+					</Grid>
 				</Toolbar>
 			</AppBar>
 		</div>
