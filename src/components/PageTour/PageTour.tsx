c375ce4df3ef8f49f82a59958a9a6576e06c0b1f
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-09 13:58:26 +1000

    feat(tour): add page tour component

---
+++ src/components/PageTour/PageTour.tsx
@@ -0,0 +1,100 @@
+import React, { ReactElement, useState } from "react"
+import Tour, { ReactourStep } from "reactour"
+import { Button } from "@material-ui/core"
+import { useLocation, useParams } from "react-router-dom"
+import hasProperty from "../../util/object/hasProperty"
+
+type PageTourProps = {
+	steps: ReactourStep[]
+}
+
+const LSKey = "react-tour-completed"
+
+/**
+ * Page tour component. Used to show highlighted areas with descriptive content
+ *
+ * @export
+ * @param {PageTourProps} { steps }
+ * @returns {ReactElement}
+ */
+export default function PageTour({ steps }: PageTourProps): ReactElement {
+	const location = useLocation()
+	const params = useParams()
+
+	/**
+	 * Strips off the beginning and ending "/"
+	 *
+	 * @returns {string}
+	 */
+	const stripPathname = (): string => location.pathname.replace(/^\/|\/$/g, "")
+
+	/**
+	 * Gets the key of the current route
+	 *
+	 * @returns {string}
+	 */
+	const getRouteValue = (): string => {
+		const re = /\/[\d]+?$/g
+		const path = stripPathname()
+
+		if (Object.keys(params).length > 0 && path.match(re)) {
+			return path.replace(re, "/:id")
+		}
+
+		return path
+	}
+
+	/**
+	 * Checks if the tour should be open by default
+	 *
+	 * @returns {boolean}
+	 */
+	const shouldBeOpen = (): boolean => {
+		const ls = window.localStorage.getItem(LSKey)
+		if (!ls) return true
+
+		const json = JSON.parse(ls)
+		return !hasProperty(json, getRouteValue())
+	}
+
+	const [open, setOpen] = useState(shouldBeOpen())
+
+	/**
+	 * Sets the localstorage value for the tour key
+	 *
+	 * @param json
+	 */
+	const setLSValue = (json: {}): void => {
+		window.localStorage.setItem(LSKey, JSON.stringify(json))
+	}
+
+	/**
+	 * Closes the tour and sets the value to not open on return
+	 *
+	 */
+	const closeTour = (): void => {
+		const ls = window.localStorage.getItem(LSKey)
+		const json = !ls ? {} : JSON.parse(ls)
+		setLSValue({
+			...json,
+			[getRouteValue()]: true,
+		})
+		setOpen(false)
+	}
+
+	/**
+	 * Opens the tour component
+	 */
+	const openTour = (): void => setOpen(true)
+
+	return (
+		<>
+			<div className="sr-only">
+				<Button onClick={openTour} id="start-a-tour">
+					Start tour
+				</Button>
+			</div>
+			<Tour steps={steps} isOpen={open} onRequestClose={closeTour} />
+		</>
+	)
+}
