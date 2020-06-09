b60dcfa4f567e3e2c102145268f2af2a8137ee61
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-09 13:58:54 +1000

    feat(tour): add example tour content to canvas create page

--- src/pages/CashFlowCanvas/canvas.tsx
+++ src/pages/CashFlowCanvas/canvas.tsx
@@ -1,10 +1,27 @@
 import React, { ReactElement } from "react"
 import { Grid } from "@material-ui/core"
+import { ReactourStep } from "reactour"
 import { PageContainer } from "../../components/Layouts"
 import PageTip from "../../components/PageTip"
 import CreateCanvasForm from "../../components/Forms/CFC/CanvasCreate"
 import FourQuestions from "../../components/HealthCheck/FourQuestions"
 import ControlPanel from "../../components/CFC/ControlPanel"
+import PageTour from "../../components/PageTour"
+
+const steps: ReactourStep[] = [
+	{
+		selector: "[data-reactour='create-a-canvas']",
+		content: "Test content 1",
+	},
+	{
+		selector: "[data-reactour='four-questions-nav']",
+		content: "Test content 2",
+	},
+	{
+		selector: "[data-reactour='canvas-control-panel']",
+		content: "Test content 3",
+	},
+]
 
 /**
  * Create a CFC page
@@ -27,6 +44,7 @@ export default function CFCCanvas(): ReactElement {
 				</Grid>
 			</PageContainer>
 			<PageTip tip="CFCanvasTip" />
+			<PageTour steps={steps} />
 		</>
 	)
 }
