3c1f9c8def3a40df55b1be38fa3060eb9957b91f
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-11 09:18:16 +1000

    feat(cfc): responsiveness tweaks for compare

--- src/components/CFCCompare/CompareTable.tsx
+++ src/components/CFCCompare/CompareTable.tsx
@@ -56,7 +56,7 @@ export default function CompareTable({
 
 	return (
 		<TableContainer>
-			<Table>
+			<Table className={cls.table}>
 				<TableHeader selectedCanvases={selectedCanvases} />
 				<TableBody>
 					<CanvasItemRow
