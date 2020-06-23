0d8ea944374dcc019f7e03aa3625f80daba6f37e
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-23 12:56:04 +1000

    feat(sf): add additional quicksnaps documents

--- src/components/SessionFiles/__config/constants.ts
+++ src/components/SessionFiles/__config/constants.ts
@@ -7,9 +7,19 @@ export const staticTitles: Record<SingleDownloadProps["id"], string> = {
 
 export const discoverTopics: DiscoverTopicsStruct[] = [
 	{
+		name: "Planning your business - Quicksnaps",
+		id: "Planning your business - Quicksnaps",
+		path: "/pdf/Planning your business - Quicksnaps.pdf",
+	},
+	{
 		name: "Business Plan - Activity",
 		id: "Business Plan - Activity",
 		path: "/pdf/BusinessPlan-Activity.pdf",
+	},
+	{
+		name: "Record Keeping - Quicksnaps",
+		id: "Record Keeping - Quicksnaps",
+		path: "/pdf/Record keeping - Quicksnaps.pdf",
 	},
 	{
 		name: "Record Keeping - Activity",
@@ -17,13 +27,38 @@ export const discoverTopics: DiscoverTopicsStruct[] = [
 		path: "/pdf/RecordKeeping-Activity.pdf",
 	},
 	{
+		name: "Funding your business - Quicksnaps",
+		id: "Funding your business - Quicksnaps",
+		path: "/pdf/Funding your business - Quicksnaps.pdf",
+	},
+	{
 		name: "Funding your business - Activity",
 		id: "Funding your business - Activity",
 		path: "/pdf/Funding-Activity.pdf",
 	},
 	{
+		name: "Managing your cash flow - Quicksnaps",
+		id: "Managing your cash flow - Quicksnaps",
+		path: "/pdf/Managing your cash flow - Quicksnaps.pdf",
+	},
+	{
+		name: "Planning your regular financial commitments - Quicksnaps",
+		id: "Planning your regular financial commitments - Quicksnaps",
+		path: "/pdf/Planning your regular financial commitments - Quicksnaps.pdf",
+	},
+	{
 		name: "Know your commitments - Activity",
 		id: "Know your commitments - Activity",
 		path: "/pdf/KnowYourCommitments-Activity.pdf",
+	},
+	{
+		name: "Tracking your performance - Quicksnaps",
+		id: "Tracking your performance - Quicksnaps",
+		path: "/pdf/Tracking your performance - Quicksnaps.pdf",
+	},
+	{
+		name: "Selling, closing and succession planning - Quicksnaps",
+		id: "Selling, closing and succession planning - Quicksnaps",
+		path: "/pdf/Selling, closing and succession planning - Quicksnaps.pdf",
 	},
 ]
