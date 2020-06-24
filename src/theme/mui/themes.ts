45570e03ef20a75f5e168942e54cb20a0c201827
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-24 11:59:22 +1000

    fix(a11y): fix orange contrast

--- src/theme/mui/themes.ts
+++ src/theme/mui/themes.ts
@@ -59,7 +59,7 @@ export const settingsTheme: ThemeOptions = {
 	...defaultTheme,
 	palette: {
 		primary: {
-			main: deepOrange.A400,
+			main: deepOrange.A700,
 		},
 		secondary: {
 			main: deepOrange["300"],
