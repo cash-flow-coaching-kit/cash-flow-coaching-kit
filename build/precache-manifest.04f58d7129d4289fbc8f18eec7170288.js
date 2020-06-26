d690b94781f05bd1943ab3c6059289e744bb29ec
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-26 16:31:54 +1000

    fix(project): commit build folder

---
+++ build/precache-manifest.04f58d7129d4289fbc8f18eec7170288.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+	{
+		revision: "5f3263876ee3bf08f90b9b540fb13334",
+		url: "/index.html",
+	},
+	{
+		revision: "f16ecabebd7402aa28fc",
+		url: "/static/css/main.7f8e66ef.chunk.css",
+	},
+	{
+		revision: "f4b6839a6fd85cd8756c",
+		url: "/static/js/0.b6fa3ab6.chunk.js",
+	},
+	{
+		revision: "512132776146470f6c46",
+		url: "/static/js/1.88ce455d.chunk.js",
+	},
+	{
+		revision: "59ac5dcd0a1186f913a8",
+		url: "/static/js/10.76bcd5d8.chunk.js",
+	},
+	{
+		revision: "1ee6b21a3a306b84e6341ef33a0bb834",
+		url: "/static/js/10.76bcd5d8.chunk.js.LICENSE.txt",
+	},
+	{
+		revision: "3145de166c28ac2be8f1",
+		url: "/static/js/2.3f863261.chunk.js",
+	},
+	{
+		revision: "28fed3bf843ce7f52e47",
+		url: "/static/js/3.c714ccd0.chunk.js",
+	},
+	{
+		revision: "7ab9585cb169cef0e836",
+		url: "/static/js/4.94bc5978.chunk.js",
+	},
+	{
+		revision: "518714f0d841061a1643",
+		url: "/static/js/5.c93e94bf.chunk.js",
+	},
+	{
+		revision: "96aca8f891e363b746ef",
+		url: "/static/js/6.12365cdd.chunk.js",
+	},
+	{
+		revision: "77b566164cdf168d12b2",
+		url: "/static/js/7.603802e6.chunk.js",
+	},
+	{
+		revision: "f16ecabebd7402aa28fc",
+		url: "/static/js/main.5ff352d9.chunk.js",
+	},
+	{
+		revision: "bfda464835eb018b0299",
+		url: "/static/js/runtime-main.fdd0ed39.js",
+	},
+])
