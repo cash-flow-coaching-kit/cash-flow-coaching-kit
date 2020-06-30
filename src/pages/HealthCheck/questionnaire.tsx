3e15c87f86f2dbf8e56c0e6b1af0ee5d5825b129
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-06-30 15:00:25 +1000

    fix(hc): content updates

--- src/pages/HealthCheck/questionnaire.tsx
+++ src/pages/HealthCheck/questionnaire.tsx
@@ -33,17 +33,23 @@ const HCQuestionnaire = (): ReactElement => {
 						<SectionTitle component="h1">Health Check</SectionTitle>
 						<Spacer />
 						<Box className="content-area">
+							<Typography>
+								Ten easy questions to learn more about the health of your
+								business.
+							</Typography>
 							<Typography className="list-describer">
 								This quick health check will help you:
 							</Typography>
 							<List component="ul" className="ul-list">
-								<Typography component="li">fill knowledge gaps</Typography>
 								<Typography component="li">
-									identify areas to improve your cash flow
+									assess your current knowledge of business cash flow
+								</Typography>
+								<Typography component="li">
+									identify areas to improve your cash flow knowledge
 								</Typography>
 							</List>
 							<Typography>
-								Your results will show you key discover topics and activities to
+								Your results will show you key topics and activities to
 								complete.
 							</Typography>
 							<Typography>
