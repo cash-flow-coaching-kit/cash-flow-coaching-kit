167d5b2acf2f40a0ae36b6891e20eea368709c23
HurricaneInteractive <hurricane.interactive@gmail.com>
2020-07-02 16:22:39 +1000

    fix(logout): enable reload on logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import React, { ReactElement } from "react"
+import React, { ReactElement, useEffect } from "react"
 import { AppRouter } from "./components/Routing"
 import { ClientProvider } from "./state/client"
 
 const App = (): ReactElement => {
+	useEffect(() => {
+		const blockReload = window.localStorage.getItem("blockReload")
+		if (!blockReload) {
+			window.localStorage.setItem("blockReload", "true")
+		}
+	}, [])
+
 	return (
 		<ClientProvider>
 			<AppRouter />
