b0ec5d6d9052f86d9713bcd0d49443969fb71066
Geoff <geoff@geofftech.com>
2020-09-22 11:13:42 +1000

    fix: add business button on IE11

--- src/components/Forms/NewClient/NewClientForm.tsx
+++ src/components/Forms/NewClient/NewClientForm.tsx
@@ -5,7 +5,15 @@ import React, {
 	useEffect,
 	ChangeEvent,
 } from "react"
-import { TextField } from "@material-ui/core"
+import {
+	TextField,
+	DialogContent,
+	DialogActions,
+	DialogContentText,
+	Typography,
+	Button,
+	makeStyles,
+} from "@material-ui/core"
 import { useFormik } from "formik"
 import { useHistory } from "react-router-dom"
 import { v4 as uuidv4 } from "uuid"
@@ -13,11 +21,27 @@ import addClient from "../../../data/client/addClient"
 import { ClientContext } from "../../../state/client"
 import { INCFormValues, INCFormErrors } from "./_config/shape"
 import { PrivateRoutes } from "../../../util/routes/routes"
+import Spacer from "../../Spacer"
 
 const MAX_LENGTH = 40
 
+// New client styling
+const useNCDStyles = makeStyles((theme) => ({
+	content: {
+		borderStyle: "solid",
+		borderColor: theme.palette.divider,
+		borderWidth: "1px 0 1px 0",
+		marginTop: theme.spacing(2),
+		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
+		"& p": {
+			margin: 0,
+		},
+	},
+}))
+
 interface NewClientFormProps {
 	closeDialog: (cb: () => void) => void
+	handleClose: any
 }
 
 /**
@@ -25,13 +49,17 @@ interface NewClientFormProps {
  *
  * @returns ReactElement
  */
-const NewClientForm = ({ closeDialog }: NewClientFormProps): ReactElement => {
+const NewClientForm = ({
+	closeDialog,
+	handleClose,
+}: NewClientFormProps): ReactElement => {
 	const { dispatch } = useContext(ClientContext)
 	const initialValues: INCFormValues = {
 		businessName: "",
 	}
 	const history = useHistory()
 	const [characters, setCharacters] = useState(0)
+	const styles = useNCDStyles()
 
 	// Defines the Formik form
 	const form = useFormik({
@@ -97,22 +125,49 @@ const NewClientForm = ({ closeDialog }: NewClientFormProps): ReactElement => {
 			onSubmit={form.handleSubmit}
 			id="add-new-business-form"
 		>
-			<TextField
-				id="businessName"
-				name="businessName"
-				label="Business name"
-				variant="outlined"
-				fullWidth
-				onChange={handleChange}
-				value={form.values.businessName}
-				error={hasError(form.errors, "businessName")}
-				helperText={
-					hasError(form.errors, "businessName")
-						? form.errors.businessName
-						: `${characters}/${MAX_LENGTH}`
-				}
-				autoFocus
-			/>
+			{/* Moved the DialogContent into the form for the submit to work in IE11 */}
+			<DialogContent>
+				<Typography>
+					It can be your business name or something memorable. If you have
+					multiple businesses, add them later.{" "}
+				</Typography>
+				<Spacer />
+				<TextField
+					id="businessName"
+					name="businessName"
+					label="Business name"
+					variant="outlined"
+					fullWidth
+					onChange={handleChange}
+					value={form.values.businessName}
+					error={hasError(form.errors, "businessName")}
+					helperText={
+						hasError(form.errors, "businessName")
+							? form.errors.businessName
+							: `${characters}/${MAX_LENGTH}`
+					}
+					autoFocus
+				/>
+			</DialogContent>
+			<DialogContent className={styles.content}>
+				<DialogContentText>
+					Remember: The kit does not save information between sessions. Use the
+					export function regularly to save your progress.
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions>
+				<Button onClick={handleClose} color="primary">
+					Cancel
+				</Button>
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					form="add-new-business-form"
+				>
+					Add business
+				</Button>
+			</DialogActions>
 		</form>
 	)
 }
--- src/content/dialog/NewClientDialog.tsx
+++ src/content/dialog/NewClientDialog.tsx
@@ -3,31 +3,11 @@ import {
 	Button,
 	Dialog,
 	DialogTitle,
-	DialogContent,
-	DialogActions,
-	DialogContentText,
-	makeStyles,
 	ButtonProps,
 	ButtonClassKey,
 	StandardProps,
-	Typography,
 } from "@material-ui/core"
 import NewClientForm from "../../components/Forms/NewClient"
-import Spacer from "../../components/Spacer"
-
-// New client styling
-const useNCDStyles = makeStyles((theme) => ({
-	content: {
-		borderStyle: "solid",
-		borderColor: theme.palette.divider,
-		borderWidth: "1px 0 1px 0",
-		marginTop: theme.spacing(2),
-		padding: `${theme.spacing(2)}px ${theme.spacing(3)}px`,
-		"& p": {
-			margin: 0,
-		},
-	},
-}))
 
 /**
  * Prop definition for the `<NewClientDialog>` component
@@ -53,7 +33,6 @@ const NewClientDialog = ({
 	...props
 }: INewClientDialogProps): ReactElement => {
 	const [open, setOpen] = useState<boolean>(false)
-	const styles = useNCDStyles()
 
 	/**
 	 * Handles the opening of the modal
@@ -100,33 +79,11 @@ const NewClientDialog = ({
 				<DialogTitle id="new-client-dialog-title">
 					First, add a business name.
 				</DialogTitle>
-				<DialogContent>
-					<Typography>
-						It can be your business name or something memorable. If you have
-						multiple businesses, add them later.{" "}
-					</Typography>
-					<Spacer />
-					<NewClientForm closeDialog={onFormCloseTrigger} />
-				</DialogContent>
-				<DialogContent className={styles.content}>
-					<DialogContentText>
-						Remember: The kit does not save information between sessions. Use
-						the export function regularly to save your progress.
-					</DialogContentText>
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={handleClose} color="primary">
-						Cancel
-					</Button>
-					<Button
-						type="submit"
-						variant="contained"
-						color="primary"
-						form="add-new-business-form"
-					>
-						Add business
-					</Button>
-				</DialogActions>
+				{/* Moved the DialogContent into the form for the submit to work in IE11 */}
+				<NewClientForm
+					closeDialog={onFormCloseTrigger}
+					handleClose={handleClose}
+				/>
 			</Dialog>
 		</>
 	)
